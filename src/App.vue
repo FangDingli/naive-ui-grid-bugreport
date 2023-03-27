e88766881ab82e554b4719cbc0c1dde5511b09af
FangDingli <1191230628@qq.com>
2023-03-27 11:24:27 +0800

    init

--- src/App.vue
+++ src/App.vue
@@ -9,17 +9,20 @@ const show2 = ref(false)
 
 <template>
   <NForm>
-    <p>使用v-if 正常切换</p>
+    <p>with v-if, it's fine to hide / show</p>
     <NSwitch v-model:value="show1"></NSwitch>
     <NGrid>
       <NFormItemGi v-if="show1">222</NFormItemGi>
     </NGrid>
-    <p>添加 layoutShiftDisabled 正常切换</p>
+    <p>with layoutShiftDisabled, it's fine to hide / show</p>
     <NSwitch v-model:value="show2"></NSwitch>
     <NGrid layoutShiftDisabled>
       <NFormItemGi v-show="show2">333</NFormItemGi>
     </NGrid>
-    <p>使用v-show且不添加 layoutShiftDisabled, 隐藏时报错, 此时切换另外两个也会报错</p>
+    <p>
+      with v-show and no layoutShiftDisabled, it's not good to hide / show, at this time, if you
+      switch the other two, it's also got error in console
+    </p>
     <NSwitch v-model:value="show"></NSwitch>
     <NGrid>
       <NFormItemGi v-show="show">111</NFormItemGi>
